adb7b858e870353eb97de1f4c78e0feca57d952a
trần phương nam <gokussj5hb@gmail.com>
2023-02-27 00:53:18 +0700

    Desgin trello Front-end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-        <h1 className='hello-world'>Hello world</h1>
+    <div className="tranphuongnam-master">
+        <nav className="navbar app">APP bar</nav>
+        <nav className="navbar board">APP board</nav>
+        <div className='board-columns'>
+        <div className='column'>
+          <header>Brainstorm</header>
+        <ul>
+          <li>
+           <img src="https://www.queerscifi.com/wp-content/uploads/2019/03/Screen-Shot-2019-03-27-at-7.12.03-AM.jpg" alt='anh cua ban'/>
+          Title:Nam Tran Phuong
+          </li>
+          <li>Hello world</li>
+          <li>Hello world</li>
+          <li>Hello world</li>
+          <li>Hello world</li>
+          <li>Hello world</li>
+         
+        
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+        <ul>
+          <li>
+           <img src="https://www.queerscifi.com/wp-content/uploads/2019/03/Screen-Shot-2019-03-27-at-7.12.03-AM.jpg" alt='anh cua ban'/>
+          Title:Nam Tran Phuong
+          </li>
+          <li>Hello world</li>
+          <li>Hello world</li>
+          <li>Hello world</li>
+          <li>Hello world</li>
+          <li>Hello world</li>
+        
+        </ul>
+        <footer>Add another card</footer>
+       </div>
+        </div>
     </div>
   );
 }
