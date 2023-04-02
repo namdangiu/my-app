a6eae81d48dd495380df41a7778d3872e45bf20a
trần phương nam <gokussj5hb@gmail.com>
2023-04-02 17:51:14 +0700

    Imlement Drag and Drop, eslint

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,21 @@
-import React from "react";
-import './Card.scss';
+import React from 'react'
+import './Card.scss'
 
 function Card(props) {
-    const {card}=props; 
-return(
-    <li className="card-item">
-        {card.cover &&  <img src={card.cover} className="card-cover" alt='anh cua ban'/>}
-        {card.title}
-   </li>
-)
+  const { card }=props
+  return (
+    <div className="card-item">
+      {card.cover &&
+      <img
+        src={card.cover}
+        // eslint-disable-next-line no-trailing-spaces
+        className="card-cover" 
+        alt='anh cua ban'
+        onMouseDown={e => e.preventDefault()}
+      />
+      }
+      {card.title}
+    </div>
+  )
 }
-export default Card;
+export default Card
